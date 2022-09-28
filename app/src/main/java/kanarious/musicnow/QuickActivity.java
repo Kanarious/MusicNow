737c0f24f496889d0126a52a8f416c91232b0be9
Keven Fletes <keven.fletes339@gmail.com>
2022-09-28 11:24:40 -0400

    Remove unused imports in QuickActivity

--- app/src/main/java/kanarious/musicnow/QuickActivity.java
+++ app/src/main/java/kanarious/musicnow/QuickActivity.java
@@ -8,7 +8,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.transition.Transition;
@@ -20,8 +19,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
